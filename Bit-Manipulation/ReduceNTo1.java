9bb0bf5a832abae991bb22edb11f6696b753f267
Ganesh <krganesh1611@gmail.com>
2021-05-26 17:47:22 +0530

    Enhance code of Reduce N to 1 for long values And also added method how to find modulus using bit-Manipulation

--- Bit-Manipulation/ReduceNTo1.java
+++ Bit-Manipulation/ReduceNTo1.java
@@ -4,7 +4,7 @@ import java.util.*;
 //Leetcode Question No. 1342
 public class ReduceNTo1 {
 
-    public static int solution(int n) {
+    public static int solution(long n) {
         //write your code here
         int res = 0;
         
@@ -12,10 +12,11 @@ public class ReduceNTo1 {
             if(n % 2 == 0){
                 n = n / 2;
             } else if(n == 3){
-                n = n + 1;
-            }else if(n % 4 == 1){
+                res += 2;
+                break;
+            }else if((n & 3) == 1){
                 n = n - 1;
-            } else if(n % 4 == 3){
+            } else if((n & 3) == 3){
                 n = n + 1;
             }
             res++;
@@ -26,7 +27,7 @@ public class ReduceNTo1 {
     
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
+		long n = scn.nextInt();
         System.out.println(solution(n));
         scn.close();
     }
