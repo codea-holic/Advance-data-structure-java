075e48e8c32dd9fdee714962b56adc00a39353b1
Ganesh <krganesh1611@gmail.com>
2021-06-02 17:36:57 +0530

    Uploded file of Remove Duplicates from Linked list Leetcode 83

---
+++ Linked-List/RemoveDuplicatesLL.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// Problem of Leetcode 83
+public class RemoveDuplicatesLL {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode removeDuplicates(ListNode head) {
+        return null;
+    }
+
+    // Input_code================================================================
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = makeList(scn.nextInt());
+
+        ListNode ans = removeDuplicates(head);
+        printList(ans);
+    }
+
+}
