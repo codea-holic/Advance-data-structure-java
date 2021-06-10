4754bee8eaff9d77438cda51ad7953e62a839d82
Ganesh <krganesh1611@gmail.com>
2021-06-10 15:16:46 +0530

    Uploaded files of Permutation of CoinChange

---
+++ Recursion-Backtracking/CoinChangePermutation1.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+// Question:- In how many ways(Permutations) you can take denomination
+//             if you have to choose a coins only once in each subset.
+public class CoinChangePermutation1 {
+
+    public static void coinChange(int[] coins, int amtsf, int tamt, String asf, boolean[] used){
+       // write your code here
+       if(amtsf > tamt){
+           return;
+       } else if(amtsf == tamt){
+           System.out.println(asf + ".");
+           return;
+       }
+       
+       
+       for(int i = 0; i < coins.length; i++){
+           if(used[i] == false){
+               used[i] = true;
+               coinChange(coins, amtsf + coins[i], tamt, asf + coins[i] + "-", used);
+               used[i] = false;
+           }
+       }
+       
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        boolean[] used = new boolean[coins.length];
+        coinChange(coins, 0, amt, "", used);
+    }
+}
