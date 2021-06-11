ed9b3ee060cd6d23141d0e5497fbb289f5daf191
Ganesh <krganesh1611@gmail.com>
2021-06-11 19:01:47 +0530

    Added new file with code of count Palindromic Subsequences

---
+++ DynamicProgramming/CountPalindromicSubsequences.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class CountPalindromicSubsequences {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        scn.close();
+        int [][] dp = new int[str.length()][str.length()];
+        
+        for(int g = 0; g < str.length(); g++){
+            for(int i = 0, j = g; j < str.length(); i++, j++){
+                if(g == 0){
+                    dp[i][j] = 1;
+                } else if (g == 1){
+                    dp[i][j] = str.charAt(i) == str.charAt(j) ? 3 : 2;
+                } else{
+                    if(str.charAt(i) == str.charAt(j)){
+                        dp[i][j] = dp[i][j - 1] + dp[i + 1][j] + 1;
+                    } else{
+                        dp[i][j] = dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+        
+        System.out.println(dp[0][str.length() - 1]);
+    }
+
+}
